79c06ad7c23a53d8aad2fa36baba79caa51ae8b5
Rohan Garg <dev.RohanGarg@gmail.com>
2016-06-14 00:21:56 +0530

    Add LineGraphActivity

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/LineGraphActivity.java
@@ -0,0 +1,19 @@
+package com.sam_chordas.android.stockhawk.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sam_chordas.android.stockhawk.R;
+
+/**
+ * Created by Rohan Garg on 14-06-2016.
+ */
+public class LineGraphActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_line_graph);
+    }
+}
