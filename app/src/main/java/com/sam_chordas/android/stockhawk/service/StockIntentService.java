9184a9f6b57860fac29489c2bfab10ea93c6066b
Rohan Garg <dev.RohanGarg@gmail.com>
2016-06-14 01:06:43 +0530

    Fix app crash when a user searches for a non-existent stock.

--- app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/service/StockIntentService.java
@@ -3,7 +3,10 @@ package com.sam_chordas.android.stockhawk.service;
 import android.app.IntentService;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
+import android.widget.Toast;
+
 import com.google.android.gms.gcm.TaskParams;
 
 /**
@@ -11,23 +14,34 @@ import com.google.android.gms.gcm.TaskParams;
  */
 public class StockIntentService extends IntentService {
 
-  public StockIntentService(){
-    super(StockIntentService.class.getName());
-  }
-
-  public StockIntentService(String name) {
-    super(name);
-  }
-
-  @Override protected void onHandleIntent(Intent intent) {
-    Log.d(StockIntentService.class.getSimpleName(), "Stock Intent Service");
-    StockTaskService stockTaskService = new StockTaskService(this);
-    Bundle args = new Bundle();
-    if (intent.getStringExtra("tag").equals("add")){
-      args.putString("symbol", intent.getStringExtra("symbol"));
+    public StockIntentService() {
+        super(StockIntentService.class.getName());
     }
-    // We can call OnRunTask from the intent service to force it to run immediately instead of
-    // scheduling a task.
-    stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args));
-  }
+
+    public StockIntentService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Log.d(StockIntentService.class.getSimpleName(), "Stock Intent Service");
+        StockTaskService stockTaskService = new StockTaskService(this);
+        Bundle args = new Bundle();
+        if (intent.getStringExtra("tag").equals("add")) {
+            args.putString("symbol", intent.getStringExtra("symbol"));
+        }
+
+        try {
+            // We can call OnRunTask from the intent service to force it to run immediately instead of
+            // scheduling a task.
+            stockTaskService.onRunTask(new TaskParams(intent.getStringExtra("tag"), args));
+        } catch (NumberFormatException e) {
+            new Handler(getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(), "Non existent stock", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
 }
